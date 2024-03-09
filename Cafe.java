79ff68abef46dcc882ed86c349c9ca713aa81e8a
Olohi John <ojohn@smith.edu>
2024-03-09 16:17:23 -0500

    Done with code. Need Javdocs, exception

--- Cafe.java
+++ Cafe.java
@@ -1,12 +1,66 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
 
-    public Cafe() {
-        System.out.println("You have built a cafe: ☕");
+public class Cafe extends Building{
+    private int nCoffeeOunces;
+    private int nSugarPackets;
+    private int nCreams;
+    private int nCups;
+
+    public Cafe(String name,String address, int nFloors, int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
+        super(name, address, nFloors);
+        //System.out.println("You have built a cafe: ☕");
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+    }
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        int value = 2;
+        if (this.nCoffeeOunces < 1 | this.nSugarPackets < 1 | this.nCreams < 1 | this.nCups < 1){
+            System.out.println("-----------\nYou need to restock. One of your items is out of stock");
+
+
+            this.restock((value * size), (value * nSugarPackets), (value * nCreams), (value * nCreams));
+        }
+        
+
+        else if (this.nCoffeeOunces < size | this.nSugarPackets < nSugarPackets | this.nCreams < nCreams | this.nCups < 1){
+            System.out.println("-----------\nYou need to restock. One of your items isn't enough to make the requested amount of coffee");
+            this.restock((value * size), (value * nSugarPackets), (value * nCreams), (value * nCreams));
+        }
+
+        System.out.println("----------------Selling Coffee!-------------");
+        this.nCoffeeOunces -= size;
+        this.nSugarPackets -= nSugarPackets;
+        this.nCreams -= nCreams;
+        this.nCups -= 1;
+        System.out.println("Coffee sold!\n------------");
+        }
+      
+
+    private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups){
+        System.out.println("----------------Restocking Cafe!-------------");
+        this.nCoffeeOunces = nCoffeeOunces;
+        this.nSugarPackets = nSugarPackets;
+        this.nCreams = nCreams;
+        this.nCups = nCups;
+        System.out.println("Cafe restocked! Proceeding to delivering requested amount of coffee\n------------");
+    }
+
+    public String toString(){
+        return (this.name + " has " + this.nCoffeeOunces + " ounces of coffee, " + this.nSugarPackets + " packs of sugar, " + this.nCreams + " splashes of cream, and " + this.nCups + " cups.");
     }
     
     public static void main(String[] args) {
-        new Cafe();
+        Cafe cc = new Cafe("Campus Cafe", "Neilson Drive", 3, 5, 3, 3, 7);
+        System.out.println(cc);
+        cc.sellCoffee(6, 3, 2);
+        System.out.println(cc);
+        cc.sellCoffee(6, 3, 2);
+        System.out.println(cc);
+  
+
     }
     
 }
--- Library.java
+++ Library.java
@@ -9,72 +9,89 @@ public class Library extends Building {
         this.collection = new Hashtable<String, Boolean>();
     }
 
-    //Reedit
-    public void addTitle(String title, Boolean available){
+    //Adds book to Library //check if it exists first also
+    public void addTitle(String title){
         System.out.println("------------\nAdding " + title);
-        this.collection.put(title, available);
+        this.collection.put(title, true);
     }
 
     //Removes book from library
-    public String removeTitle(String title, Boolean available){
-      if (available.equals(true)){
+    public String removeTitle(String title){
+      if (this.collection.get(title) == true){
         System.out.println("------------\nRemoving " + title);
-        this.collection.remove(title, available);
-        return (title + "removed from library");
+        this.collection.remove(title);
+        return ("------------\n" + title + " removed from library"); 
       }
       else{
-        return(title + "not in library.");
+        return("------------\n" + title + " not in library.");
       }
     }
 
-    //updates availability to false during check out
-    public void checkOut(String title, Boolean available){
-      if (available.equals(true)){
-        System.out.println("------------\nChecking out " + title);
-        this.collection.replace(title, available, false);
-        System.out.println(title + " checked out from library");
+    //updates the value of the title if available from true to false during check out
+    public void checkOut(String title){
+      //System.out.println(this.collection.keySet());
+      if (this.collection.containsKey(title)){
+        if (this.collection.get(title) == true){
+          System.out.println("------------\nChecking out " + title);
+          this.collection.replace(title, true, false);
+        
+          System.out.println("------------\n" + title + " checked out of library");
+        }
+        else{
+          System.out.println(title + " not available now");
+        }
       }
       else{
-        System.out.println(title + " not in library.");
+        System.out.println(this.name + " does not have " + title);
       }
     }
 
     //Returns a book and updates its availability to true
-    public void returnBook(String title, Boolean available){
-      if (available.equals(false)){
+    public void returnBook(String title){
+      if (this.collection.containsKey(title)){
         System.out.println("------------\nChecking in " + title);
-        this.collection.replace(title, available, true);
-        System.out.println(title + " returned to library");
+        this.collection.replace(title, false, true);
+        System.out.println("------------\n" + title + " returned to library");
       }
       else{
         System.out.println(title + " not in library.");
       }
     }
 
-    // public String toString(){
-    //   for (String book:this.collection(key)){
+    public boolean containsTitle(String title){
+      if (this.collection.containsKey(title)){
+        System.out.println("-----------\n" + title + " is available at " + this.name);
+        return true;
+      }
+      else{
+        System.out.println(title + " is not available at " + this.name);
+        return false;
+      }
+    }
 
-    //   }
-    // }
 
-
-
-
-
-
-
-
+    public String toString(){
+      this.collection.keys();
+      System.out.println("--------------------------------");
+      String library = "The books at " + this.name + " are: ";
+      for (String book : this.collection.keySet()){
+        library += "\n" + book;
+      }
+      return library;
+    }
 
   
     public static void main(String[] args) {
         Library Neilson = new Library("Neilson", "Massachusetts", 5);
-        Neilson.addTitle("Golden Gulag by Rotkins", true);
-        Neilson.addTitle("Empower by Daniels", true);
-        Neilson.addTitle("Labouring Women by Smallwood Stephanie", true);
-        Neilson.removeTitle("Golden Gulag by Rotkins", true);
-        Neilson.checkOut("Golden Gulag by Rotkins", true);
-        Neilson.returnBook("Golden Gulag by Rotkins", false);
-
+        Neilson.addTitle("Golden Gulag by Rotkins");
+        Neilson.addTitle("Empower by Daniels");
+        Neilson.addTitle("Labouring Women by Smallwood Stephanie");
+        Neilson.checkOut("Golden Gulag by Rotkins");
+        System.out.println(Neilson.removeTitle("Golden Gulag by Rotkins"));
+        Neilson.containsTitle("Empower by Daniels");
+        Neilson.checkOut("Empower by Daniels");
+        Neilson.returnBook("Golden Gulag by Rotkins");
+        System.out.println(Neilson);;
 
     }
   
