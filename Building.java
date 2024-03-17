3bfd37129a70041595618b1f404428da16f82e92
Olohi John <ojohn@smith.edu>
2024-03-17 18:33:21 -0400

    Added javDoc comments to the building class

--- Building.java
+++ Building.java
@@ -4,6 +4,12 @@ public class Building {
     protected String address = "<Address Unknown>";
     protected int nFloors = 1;
 
+    /**
+     * Constructor, initializes the name, address, and number of floors contained in the building
+     * @param name
+     * @param address
+     * @param nFloors
+     */
     public Building(String name, String address, int nFloors) {
         if (name != null) { this.name = name; }
         if (address != null) { this.address = address; } 
@@ -12,23 +18,35 @@ public class Building {
         }
         this.nFloors = nFloors;
     }
-
+    /**
+     * Getter for name of house
+     * @return
+     */
     public String getName() {
         return this.name;
     }
-
+    /**
+     * Getter for address of house
+     * @return
+     */
     public String getAddress() {
         return this.address;
     }
-
+    /**
+     * Getter for number of floors 
+     * @return
+     */
     public int getFloors() {
         return this.nFloors;
     }
-
+    /**
+     * Prints name of building together with its attributes
+     */
     public String toString() {
         return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
     }
-
+    
+    //Main function for testing the program's functionality
     public static void main(String[] args) {
         Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
         System.out.println(fordHall);
